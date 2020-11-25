15eede678969c9996fc662c2084e9be0fe860ed7
Anton Ohorodnyk <anton@ohorodnyk.name>
2020-11-24 23:03:02 -0800

    Add has/set flag funcs for int8

---
+++ flags_int8.go
@@ -0,0 +1,43 @@
+package gobitflags
+
+import (
+	"errors"
+	"math"
+)
+
+func HasFlagInt8(flags int8, flag uint8) (bool, error) {
+	if flag > FlagMaxUint8 {
+		return false, errors.New(ErrorMsgOutOfRange)
+	}
+
+	conv := flagInt8(flag)
+	return flags&conv == conv, nil
+}
+
+func SetFlagInt8(flags int8, flag uint8, val bool) (int8, error) {
+	if flag > FlagMaxUint8 {
+		return flags, errors.New(ErrorMsgOutOfRange)
+	}
+
+	hasFlag, err := HasFlagInt8(flags, flag)
+	if err != nil {
+		return flags, err
+	}
+
+	if hasFlag == val {
+		return flags, nil
+	}
+
+	conv := flagInt8(flag)
+	ret := flags ^ conv
+	return ret, nil
+}
+
+func flagInt8(flag uint8) int8 {
+	var ret int8 = math.MinInt8
+	if flag < FlagMaxUint8 {
+		ret = 1 << flag
+	}
+
+	return ret
+}
---
+++ flags_int8_test.go
@@ -0,0 +1,155 @@
+package gobitflags_test
+
+import (
+	"fmt"
+	"github.com/aohorodnyk/gobitflags"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestHasFlagInt8(t *testing.T) {
+	for idx, prov := range providerHasFlagInt8() {
+		t.Run(fmt.Sprintf("TestHasFlagInt8%d", idx), func(t *testing.T) {
+			actual, err := gobitflags.HasFlagInt8(prov.flags, prov.flag)
+
+			assert.Equal(t, prov.expected, actual)
+			assert.Equal(t, prov.err, err)
+		})
+	}
+}
+
+func TestSetFlagInt8(t *testing.T) {
+	for idx, prov := range providerSetFlagInt8() {
+		t.Run(fmt.Sprintf("TestSetFlagInt8%d", idx), func(t *testing.T) {
+			actual, err := gobitflags.SetFlagInt8(prov.flags, prov.flag, prov.val)
+
+			assert.Equal(t, prov.expected, actual)
+			assert.Equal(t, prov.err, err)
+		})
+	}
+}
+
+type providerTypeHasFlagInt8 struct {
+	flags    int8
+	flag     uint8
+	expected bool
+	err      error
+}
+
+type providerTypeSetFlagInt8 struct {
+	flags    int8
+	flag     uint8
+	val      bool
+	expected int8
+	err      error
+}
+
+func providerHasFlagInt8() []providerTypeHasFlagInt8 {
+	return []providerTypeHasFlagInt8{
+		{
+			flags:    0,
+			flag:     0,
+			expected: false,
+			err:      nil,
+		},
+		{
+			flags:    52,
+			flag:     7,
+			expected: false,
+			err:      nil,
+		},
+		{
+			flags:    math.MinInt8,
+			flag:     7,
+			expected: true,
+			err:      nil,
+		},
+		{
+			flags:    math.MinInt8,
+			flag:     3,
+			expected: false,
+			err:      nil,
+		},
+		{
+			flags:    -1,
+			flag:     3,
+			expected: true,
+			err:      nil,
+		},
+		{
+			flags:    -1,
+			flag:     7,
+			expected: true,
+			err:      nil,
+		},
+	}
+}
+
+func providerSetFlagInt8() []providerTypeSetFlagInt8 {
+	return []providerTypeSetFlagInt8{
+		{
+			flags:    0,
+			flag:     0,
+			val:      false,
+			expected: 0,
+			err:      nil,
+		},
+		{
+			flags:    0,
+			flag:     0,
+			val:      true,
+			expected: 1,
+			err:      nil,
+		},
+		{
+			flags:    1,
+			flag:     1,
+			val:      true,
+			expected: 3,
+			err:      nil,
+		},
+		{
+			flags:    53,
+			flag:     1,
+			val:      true,
+			expected: 55,
+			err:      nil,
+		},
+		{
+			flags:    53,
+			flag:     0,
+			val:      false,
+			expected: 52,
+			err:      nil,
+		},
+		{
+			flags:    -1,
+			flag:     5,
+			val:      true,
+			expected: -1,
+			err:      nil,
+		},
+		{
+			flags:    -1,
+			flag:     5,
+			val:      false,
+			expected: -33,
+			err:      nil,
+		},
+		{
+			flags:    -1,
+			flag:     7,
+			val:      false,
+			expected: 127,
+			err:      nil,
+		},
+		{
+			flags:    math.MinInt8,
+			flag:     7,
+			val:      false,
+			expected: 0,
+			err:      nil,
+		},
+	}
+}
