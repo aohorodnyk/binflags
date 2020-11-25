6159de3094d9882e30a454a8ccc5f184eb7b75c6
Anton Ohorodnyk <anton@ohorodnyk.name>
2020-11-25 14:13:54 -0800

    #1 Add FlagMap for index -> value resolving

--- const.go
+++
@@ -1,8 +0,0 @@
-package gobitflags
-
-const FlagMaxInt8 = 7
-const FlagMaxInt16 = 15
-const FlagMaxInt32 = 31
-const FlagMaxInt64 = 63
-
-const ErrorMsgOutOfRange = "flag is out of range"
---
+++ consts.go
@@ -0,0 +1,8 @@
+package gobitflags
+
+const FlagMaxInt8 uint8 = 7
+const FlagMaxInt16 uint8 = 15
+const FlagMaxInt32 uint8 = 31
+const FlagMaxInt64 uint8 = 63
+
+const ErrorMsgOutOfRange = "flag is out of range"
