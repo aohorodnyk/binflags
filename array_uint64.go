1a2fd3f199cadecba51ac651b0ba387d5b06d8a4
Anton Ohorodnyk <anton@ohorodnyk.name>
2020-11-26 12:38:40 -0800

    #1 Add Int64 and Uint64

---
+++ array_uint64.go
@@ -0,0 +1,35 @@
+package gobitflags
+
+import "errors"
+
+func HasFlagArrayUint64(flags []uint64, flag uint64) bool {
+	if len(flags) == 0 {
+		return false
+	}
+
+	idx, bit := flagExt(flag, FlagMaxInt64)
+	if len(flags) <= int(idx) {
+		return false
+	}
+
+	return HasFlagUint64(flags[idx], bit)
+}
+
+func SetFlagArrayUint64(flags []uint64, flag uint64, set bool) error {
+	if flags == nil {
+		return errors.New(ErrorMsgFlagsArrayNil)
+	}
+
+	if HasFlagArrayUint64(flags, flag) == set {
+		return nil
+	}
+
+	idx, bit := flagExt(flag, FlagMaxInt64)
+	if len(flags) <= int(idx) {
+		return errors.New(ErrorMsgOutOfRange)
+	}
+
+	var err error
+	flags[idx], err = SetFlagUint64(flags[idx], bit, set)
+	return err
+}
